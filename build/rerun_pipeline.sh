bd90f9babcf285c28a2b449fa02a01959f8a9cf5
mmckay18 <mckaymyle@gmail.com>
2022-11-02 13:58:52 -0700

    Remove old pipeline files

--- build/rerun_pipeline.sh
+++
@@ -1,3 +0,0 @@
-wingspipe delete -y
-wingspipe init -w /Users/mmckay/phd_projects/phat_pipeline_dev/phat_pypipeline_repo/build/ -i /Users/mmckay/phd_projects/phat_pipeline_dev/phat_pypipeline_repo/input/ -c /Users/mmckay/phd_projects/phat_pipeline_dev/phat_pypipeline_repo/config/default.conf
-wingspipe run

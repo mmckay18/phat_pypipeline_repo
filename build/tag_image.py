d92a9ef54410e560340a7fa2c8d290d158408045
mmckay18 <mckaymyle@gmail.com>
2022-12-01 13:09:12 -0800

    Added FITS parameters to proc_dp in tag_image

--- build/tag_image.py
+++ build/tag_image.py
@@ -37,61 +37,68 @@ def run_pypiper():
     return this_config, this_job_id
 
 
-def copy_rawdp_proc():
-
-    """
-
-    Copy raw dataproduct objects to the proc directory for tagging
-
-    """
-
-    # my_dp = my_input.dataproduct(filename, group)
-
-
 if __name__ == "__main__":
     my_pipe = wp.Pipeline()
     my_job = wp.Job()
     my_input = my_pipe.inputs[0]
     my_targets = my_input.targets
+    # my_config = wp.configuration("default.conf")
+    # my_job.logprint(f"{my_config.parameters.name}")
 
-    my_job.logprint(f"Targets: {my_targets.datapath}")
+    # my_job.logprint(f"Targets: {my_targets.datapath}")
     for target in my_targets.datapath:
-        my_job.logprint(f"{target}/*.fits")
+        # my_job.logprint(f"{target}/*.fits")
         fits_path = f"{target}/raw_default/*.fits"
         list_of_dp = glob.glob(fits_path)  # List of raw_default FLC fits files
-        my_job.logprint(f"List of dp in Target: {list_of_dp}")
+        # my_job.logprint(f"List of dp in Target: {list_of_dp}")
 
-        # create dataproduct object in target
-        for dp_fname in list_of_dp:
-            my_dp = my_input.dataproduct(filename=dp_fname, group="raw")
-            my_job.logprint(f" Dataproduct path {my_dp}")
+        # [done] Step 1: Copy images associated with the dataproducts from raw default to the proc directory - or copy the dataproducts from the config file?
+        for dp_fname_path in list_of_dp:
+            dp_fname = dp_fname_path.split("/")[-1]
+            # my_job.logprint(dp_fname)
+            # my_job.logprint({dp_fname_path})
+            my_rawdp = my_input.dataproduct(filename=dp_fname, group="raw")
+            # my_job.logprint(f" Dataproduct path {my_rawdp}")
+            proc_path = f"{target}/proc_default/"
+            # my_job.logprint(f"{type(proc_path)}")
+            # my_job.logprint(f"Proc Directory {str(proc_path)}")
+            my_job.logprint(f"Copy {dp_fname} -> {proc_path}")
+            my_rawdp.make_copy(path=proc_path, group="proc")
 
-        # Make a copy of the dataproduct to the proc dirtectory of the target
-        # proc_path = f"{target}/proc_default/"
-        # shutil.copy2(rawdp.path, procdp.path)
+            # Make the proc directory a dataproduct
+            raw_hdu = fits.open(dp_fname_path)
+            FILENAME = raw_hdu[0].header["FILENAME"]
+            RA = raw_hdu[0].header["RA_TARG"]
+            DEC = raw_hdu[0].header["DEC_TARG"]
+            TELESCOP = raw_hdu[0].header["TELESCOP"]
+            ORINT = raw_hdu[0].header["P1_ORINT"]
+            EXPTIME = raw_hdu[0].header["EXPTIME"]
+            EXPFLAG = raw_hdu[0].header["EXPFLAG"]
+            CAM = raw_hdu[0].header["INSTRUME"]
+            FILTER = raw_hdu[0].header["FILTER"]
 
-        # my_job.logprint(f"Proc dirtectory: {proc_path}")
-        # my_dp.make_copy(path=proc_path)
-        # my_job.logprint(f"Copying {dp_fname} to {target}/proc_default/*.fits")
-
-    # my_job.logprint(f"Targets: {my_targets.dataraws}")
-    # my_input = wp.Input(my_pipe, my_pipe.input_root)
-    # my_input = my_pipe.inputs[0]
-    # my_targets_list = my_pipe.inputs.target()
-    # my_target_list = wp.Target(my_input)
-    # my_job.logprint(f"Targets: {my_target_list}")
+            my_procdp = my_input.dataproduct(
+                filename=dp_fname,
+                group="proc",
+                options={
+                    "filename": FILENAME,
+                    "ra": RA,
+                    "dec": DEC,
+                    "Telescope": TELESCOP,
+                    "ORIENTATION": ORINT,
+                    "Exptime": EXPTIME,
+                    "Expflag": EXPFLAG,
+                    "cam": CAM,
+                    "filter": FILTER,
+                },
+            )
+            my_job.logprint(
+                f" Dataproduct path {my_procdp.filename} {my_procdp.options}"
+            )
 
     # TODO:
-    # * Step 1: Copy images associated with the dataproducts from raw default to the proc directory - or copy the dataproducts from the config file?
-
-    # * Work In Progress: *
-    # Copy raw dataproduct files to proc directory
-    # for dp_filename in rawdp_fn_list:
-    #     my_dp = my_input.dataproduct(dp_filename, group='raw')
-    #     my_dp.make_copy(path=wp.Pipeline()) #point to path in string format, also point to config file - get proc directory from config file
-    #     my_config.confpath
-
     # * Step 2: print target configuration file (default.config)
+
     # * Step 3: Use the default configuration to tag the associated images of a target
     # * Step 4: Fire next task
 
@@ -100,43 +107,3 @@ if __name__ == "__main__":
     this_config, this_job_id = run_pypiper()
 
     print("CONFIG ID:", this_config, "JOB ID:", this_job_id)
-
-
-# TODO: !REMOVE THIS -> using for reference
-# def sort_input_dataproduct(my_input):
-#     """ """
-
-#     for my_rawdp in my_input.rawdataproducts:
-
-#         my_rawdp_fits_path = my_rawdp.path
-
-#         # 1. Grab fitsfile header info directly from the dataproduct
-#         hdu = fits.open(my_rawdp_fits_path)
-#         prop_id = str(hdu[0].header["PROPOSID"])
-#         targname = hdu[0].header["TARGNAME"]
-#         hdu.close()
-
-#         # ! Field target name
-#         target_name = prop_id + "_" + targname
-
-#         # Create a new test target
-#         # TODO my_target = my_input.target('target', rawdps_to_add='raw.dat')
-#         my_target = my_input.target(name=target_name, rawdps_to_add=my_rawdp)
-
-#         # * Grab default configuration
-#         # ? my_config = my_target.configurations[<name_of_config>]
-#         my_config = my_target.configurations["default"]
-#         # my_job.logprint(
-#         #     f"Target: {my_target.name} Config: {my_config.name}, inputname: {my_rawdp.filename}"
-#         # )
-
-#         # my_job.logprint(f""
-#         # create new dataproduct with the name of the input image
-#         _dp = my_config.dataproduct(
-#             filename=my_rawdp.filename,
-#             relativepath=my_config.rawpath,
-#             group="raw",
-#             subtype="image",
-#         )
-#         # my_job.logprint(f"DP: {_dp.filename}") # * for debugging purposes
-#     return _dp
